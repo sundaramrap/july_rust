74f7cbff70200c9273325ff24bcfe5b94b20f54b
sundaramrap <pragya.learn.28@gmail.com>
2025-09-02 22:08:23 +0530

    'variblesOfRust'

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+// fn main() {
+//     println!("Hello, world!");
+// }
+
+
+//Basic Varibles in Rust
+
+// numbers
+//i32--> decide the number size , i means signed integer..since rust is an memeory based language
+fn main(){
+    let x: i32 =1;
+    println!("{}",x);
 }
